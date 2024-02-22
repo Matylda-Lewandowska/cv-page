254e0824104aab7552f4accdb1b40a3885fde4ff
Matylda Lewandowska <matylda.lewandowska111@gmail.com>
2024-02-22 14:51:53 +0100

    add Header and scss

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import './header.scss';
 
 export const Header = () => {
+  const navList = [
+    {
+      title: 'IT',
+      link: 'http://localhost:3000/#/IT'
+    },
+    {
+      title: 'Art',
+      link: 'http://localhost:3000/#/Art'
+    },
+    {
+      title: 'About',
+      link: 'http://localhost:3000/#/about'
+    }
+  ];
+
   return (
-    <div>Header</div>
+    <>
+      <div>Header</div>
+      <nav className='nav'>
+        <div>
+          <div>
+            Logo Here or smth
+          </div>
+
+        </div>
+
+        <div className='nav__list'>
+          {navList.map(navItem => {
+            return (
+              <a href={navItem.link}>
+                <div className='nav__list-item'>
+                  {navItem.title}
+                </div>
+              </a>
+            );
+          })
+
+          }
+        </div>
+      </nav>
+    </>
   );
 };
