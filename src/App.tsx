6f5902ac98670e3f55509955e135c422f368c6bf
Matylda Lewandowska <matylda.lewandowska111@gmail.com>
2024-02-23 18:07:55 +0100

    add more routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import './App.css';
 import { Home } from './pages/Home/Home';
 import { MainProvider } from './context/MainContext';
+import { About } from './pages/About/About';
+import { ITHub } from './pages/ITHub/ITHub';
+import { ArtHub } from './pages/ArtHub/ArtHub';
+import { ITTeckStack } from './pages/ITTeckStack/ITTeckStack';
+import { ITProjects } from './pages/ITProjects/ITProjects';
+import { ITFunctions } from './pages/ITFunctions/ITFunctions';
 
 function App () {
   return (
@@ -10,6 +16,17 @@ function App () {
       <Router>
         <Routes>
           <Route path='home' element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='it' element={<ITHub />} >
+          </Route>
+          <Route path='it/tech-stack' element={<ITTeckStack />} />
+          <Route path='it/projects' element={<ITProjects />}>
+          </Route>
+          <Route path='it/projects/:id' element={<ITProjects />} />
+          <Route path='it/functions' element={<ITFunctions />}>
+          </Route>
+          <Route path='it/projects/:id' element={<ITFunctions />} />
+          <Route path='art' element={<ArtHub />} />
           <Route path='*' element={<Navigate replace to="/home" />} />
 
         </Routes>
