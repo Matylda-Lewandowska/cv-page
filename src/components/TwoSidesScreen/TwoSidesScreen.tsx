e02f0bb5a19d925ce973ceb17481a42fdc7db184
Matylda Lewandowska <matylda.lewandowska111@gmail.com>
2024-02-28 17:29:06 +0100

    fix some naming issues

--- src/components/TwoSidesScreen/TwoSidesScreen.tsx
+++ src/components/TwoSidesScreen/TwoSidesScreen.tsx
@@ -13,8 +13,8 @@ export const TwoSidesScreen: React.FC<TwoSidesScreenProps> = ({ titleLeft, title
         <div className='tSS__title'>TwoSidesScreen</div>
 
         <div className='tSS__choice choice'>
-          <div className='choice__first choice__first:hover'>{titleLeft}</div>
-          <div className='choice__second choice__second:hover'>{titleRight}</div>
+          <div className='choice__first bg-blue-main choice__first:hover'>{titleLeft}</div>
+          <div className='choice__second bg-purple-main choice__second:hover'>{titleRight}</div>
         </div>
 
       </div>
